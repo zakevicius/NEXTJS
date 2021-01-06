094af388dbb042bbdf8c5e48b6df422139e60e37
zakevicius <m.zakevicius@gmail.com>
2021-01-06 17:01:15 +0200

    comment out locale subpaths

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,12 +1,11 @@
-const { nextI18NextRewrites } = require("next-i18next/rewrites");
+// const { nextI18NextRewrites } = require("next-i18next/rewrites");
 
-const localeSubpaths = {};
+// const localeSubpaths = {};
 
 module.exports = {
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
-    localeDetection: false,
   },
   // rewrites: async () => nextI18NextRewrites(localeSubpaths),
   // publicRuntimeConfig: {
