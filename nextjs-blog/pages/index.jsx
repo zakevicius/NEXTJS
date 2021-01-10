cc179bbed98d95fffb84a7028f3c07800c1d8e70
Marius <m.zakevicius@gmail.com>
2021-01-10 21:29:09 +0200

    Check if right credentials

--- nextjs-blog/pages/index.jsx
+++ nextjs-blog/pages/index.jsx
@@ -100,7 +100,7 @@ export const getStaticProps = async ({ preview, locale }) => {
     console.log(translations);
   } catch (err) {
     console.log('Error receiving');
-    console.log(process.env.S3_SECRET_KEY);
+    console.log('this is secret key: ' + process.env.S3_SECRET_KEY);
     console.log(process.env.S3_ACCESS_KEY);
     console.log(process.env.S3_BUCKET);
     console.log(err);
